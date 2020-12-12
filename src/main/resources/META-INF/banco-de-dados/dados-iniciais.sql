b679beb6eef5835abf964c2fd49d8f3561d9edea
Mailson Fernando Pereira <mailsonfp.dev@gmail.com>
2020-12-12 15:55:43 -0300

    Aula 9.31 a 9.40

--- src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
+++ src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
@@ -8,18 +8,20 @@ insert into Cliente (id, nome, sexo) values (2, 'Marcos Mariano', 'MASCULINO');
 insert into pedido (id, cliente_id, data_criacao, total, status) values (1, 1,  date_sub(sysdate(), interval 5 day), 2398, 'AGUARDANDO');
 insert into pedido_item (pedido_id, produto_id, preco_produto, quantidade) values (1, 1, 499, 2);
 insert into pedido_item (pedido_id, produto_id, preco_produto, quantidade) values (1, 3, 1400, 1);
+insert into pagamento (pedido_id, status, tipo_pagamento, numero_cartao, codigo_barras) values (1, 'RECEBIDO', 'BOL', '0123', null);
 
 insert into pedido (id, cliente_id, data_criacao, total, status) values (2, 1, sysdate(), 499.0, 'AGUARDANDO');
 insert into pedido_item (pedido_id, produto_id, preco_produto, quantidade) values (2, 1, 499, 1);
 insert into nota_fiscal (pedido_id, xml, data_emissao) values (2, '<xml />', sysdate());
+insert into pagamento (pedido_id, status, numero_cartao, tipo_pagamento) values (2, 'PROCESSANDO', '123', 'CRT');
 
 insert into pedido (id, cliente_id, data_criacao, total, status) values (3, 1, date_sub(sysdate(), interval 4 day), 3500.0, 'PAGO');
 insert into pedido_item (pedido_id, produto_id, preco_produto, quantidade) values (3, 4, 3500, 1);
+insert into pagamento (pedido_id, status, tipo_pagamento, numero_cartao, codigo_barras) values (3, 'RECEBIDO', 'BOL', null, '8910');
 
 insert into pedido (id, cliente_id, data_criacao, total, status) values (4, 2, date_sub(sysdate(), interval 2 day), 499.0, 'PAGO');
 insert into pedido_item (pedido_id, produto_id, preco_produto, quantidade) values (4, 1, 499, 1);
-
-insert into pagamento (pedido_id, status, numero_cartao, tipo_pagamento) values (2, 'PROCESSANDO', '123', 'CRT');
+insert into pagamento (pedido_id, status, tipo_pagamento, numero_cartao, codigo_barras) values (4, 'PROCESSANDO', 'CRT', '1112', null);
 
 
 insert into categoria (id, nome) values (1, 'Eletrodomésticos');
